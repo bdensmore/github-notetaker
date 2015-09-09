7961cbedd66875c1788af25d4505d5ff17193f75
Ben Densmore <bdensmore@densmore.net>
2015-09-09 13:43:13 +0000

    Add axiom for hitting the github API

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,7 +1,14 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
+var helpers = require('../utils/helpers');
 
 var Profile = React.createClass({
+  mixins: [Router.State, ReactFireMixin],
   getInitialState: function(){
     return {
       notes: [],
@@ -9,17 +16,41 @@ var Profile = React.createClass({
       repos: []
     }
   },
+  componentDidMount: function() {
+    this.ref = new Firebase('https://bdensmore-notetaker.firebaseio.com/');
+    var childRef = this.ref.child(this.getParams().username);
+    this.bindAsArray(childRef, 'notes');
+
+    helpers.getGithubInfo(this.getParams().username)
+      .then(function(dataObj){
+        this.setState({
+          bio: dataObj.bio,
+          repos: dataObj.repos
+        }.bind(this));
+      });
+  },
+  componentWillUnmount: function() {
+    this.unbind('notes')
+  },
+  handleAddNote: function(newNote){
+    //console.log(this.state.notes.concat([newNote]));
+    this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote]));
+  },
   render: function() {
+    var username = this.getParams().username;
     return (
       <div className="row">
         <div className="col-md-4">
-          User Profile Component
+          <UserProfile username={username} bio={this.state.bio}/>
         </div>
         <div className="col-md-4">
-          Repos Component
+          <Repos username={username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          Notes Component
+          <Notes
+            username={username}
+            notes={this.state.notes}
+            addNote={this.handleAddNote} />
         </div>
       </div>
     )
